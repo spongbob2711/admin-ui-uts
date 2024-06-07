ce16662e906968d8a4f87de5d2c9309218a907b3
JevanBintoro <111202113433@mhs.dinus.ac.id>
2024-06-07 14:37:13 +0700

    update firebase

---
+++ src/firebase.jsx
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: "store-tutorial-16073.firebaseapp.com",
+  projectId: "store-tutorial-16073",
+  storageBucket: "store-tutorial-16073.appspot.com",
+  messagingSenderId: "128378189599",
+  appId: "1:128378189599:web:a277fb7ad68f977304e22a",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
